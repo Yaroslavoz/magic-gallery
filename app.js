830d6856f6628e34a66c032b4a97fa11e76841aa
yaroslavoz <yaroslavozv@gmail.com>
2021-09-23 11:53:03 +0300

    feat: js upgrade

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const slides = document.querySelectorAll('.slide')
 
-for (let slide of slides){
-  slide.addEventListener('click', ()=>{
-    clearActive()
-    slide.classList.add('active')
-  })
+function slidesPlugin(activeSlide = 0){
+  const slides = document.querySelectorAll('.slide')
+
+  slides[activeSlide].classList.add('active')
+
+  for (let slide of slides){
+    slide.addEventListener('click', ()=>{
+      clearActive()
+      slide.classList.add('active')
+    })
+  }
+
+
+  function clearActive(){
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
+
 }
 
-
-function clearActive(){
-  slides.forEach((slide) => {
-    slide.classList.remove('active')
-  })
-}
+slidesPlugin(1)
